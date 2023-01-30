9b2cef32a647bf3cd62cceb576ce56ddac312a3b
GetBitOutdoors <ryan@getbitoutdoors.com>
2023-01-30 00:50:10 -0500

    Code 1.0 html/css for Resume site #milestone.

--- src/cv/data.clj
+++ src/cv/data.clj
@@ -14,6 +14,7 @@
    "Additional Skills"
    "Functional Programming \u2028Clojure/Javascript\n\nREPL / Test Driven Development\u2028\u2028Data Modeling, Specification, Validation, Transformation\n\nData Visualization, Dashboards\n\nShell Scripting, CLI Tools, Git, Regex\n\nGraphQL, REST / JSON API\n\nAdvanced reactive UI state management\n\nGraphics coding, Adobe Creative Suite"})
 
+;; parse personal info str
 (comment
   (->
     "Address\n1821 Amherst Ave.\nOrlando, FL\n\nPhone\n917-743-9113\n\nE-mail\nryanmarti@gmail.com\n\nLinkedIn\nlinkedin.com/in/ryan-martin-1bb63511/\n\nGitHub\u2028github.com/chromalchemy\n"
@@ -24,6 +25,15 @@
       flatten
       (setval (nthpath 2) NONE)
       (apply hash-map))))
+
+;; parse skill keywords
+(comment
+  (->
+    "Functional Programming Clojure/Javascript REPL / Test Driven Development Data Modeling, Specification, Validation, Transformation\n\nData Visualization, Dashboards\n\nShell Scripting, CLI Tools, Git, Regex\n\nGraphQL, REST / JSON API\n\nAdvanced reactive UI state management\n\nGraphics coding, Adobe Creative Suite"
+    (string/split #"\n")
+    (->>
+      (filterv #(not= % "")))))
+
 
 (comment
   (->> text-copied-from-resume-doc))
@@ -75,8 +85,8 @@
 (def skills-summary
   ;:skills-summary #_"Skills Summary"
   {"Clojure Programming"
-   ["Wrote 30,000 lines of Clojure to support Get Bit Outdoors ERP system, aggregating data from 18 different silos and various formats into custom master database, verifying data integrity across 50K objects, generating reports, and coordinating complex CRUD operations to update system over different application API’s."
-    "Developed custom “Real Time Inventory” integration, reducing customer service calls 70%."
+   ["Wrote 30,000 lines of Clojure to support Get Bit Outdoors ERP system, aggregating data from 18 different silos and various formats into custom master database, verifying data integrity across 50K objects, generating reports, and coordinating complex CRUD operations to update system over different application API's."
+    "Developed custom Real Time Inventory integration, reducing customer service calls 70%."
     "Scripted browser with WebDriver to automate record updates on an internal web app that lacked an API. Scraped catalog data from vendor websites."
     "Built reactive ClojureScript Todo app with custom in-memory graph datastore and Firebase integration. "],
 
@@ -91,12 +101,74 @@
 
 (def experience
   #_"Experience"
-  {"Graphic Designer, VGS Graphic Systems 2006-2007" "Prepared graphic files for a large format Durst printer and a Zune plotter., scripting a process to generate variations on a design. Cut and manually prepared vinyl signs for installation.",
-   "Painter, Orlando, FL 2007-Present" "Created, exhibited, and sold classical modern paintings in watercolor, acrylic, and oil color. Created a Linseed oil refinement system and produced custom oil paint mediums that support \"Old Master\" texture and technique.",
+  {"Software Developer, Get Bit Outdoors, Oviedo, FL 2019-Present" "Using Clojure to implement solutions at all levels of the software stack.",
    "Graphic Designer/Web Developer, TinyBoxer, TopRank Web, Orlando, FL  2010-2019" "Developed on 30+ websites on a contract basis, programming front-end web views and display logic. Used leading web CMS platforms, designed data schemas, configured web servers, optimized view code for performance and SEO. Consulted clients on information design, created working wireframe prototypes, crafted and batch-processed media assets.",
-   "Software Developer, Get Bit Outdoors, Oviedo, FL 2019-Present" "Using Clojure to implement solutions at all levels of the software stack.",
+   "Painter, Orlando, FL 2007-Present" "Created, exhibited, and sold classical modern paintings in watercolor, acrylic, and oil color. Created a Linseed oil refinement system and produced custom oil paint mediums that support \"Old Master\" texture and technique.",
+   "Graphic Designer, VGS Graphic Systems 2006-2007" "Prepared graphic files for a large format Durst printer and a Zune plotter., scripting a process to generate variations on a design. Cut and manually prepared vinyl signs for installation.",
    "IT Technician, BJM & Associates, Orlando, FL 2001-2005" "Purchased, installed, and maintained PC applications, hardware, and network solutions for medium sized Engineering firm."})
 
 (def education
   #_"Education"
   "New College, Sarasota, FL, 1997-2000")
+
+(def skill-keywords
+  ["REPL / Test Driven Development"
+   "Data Modeling, Specification, Validation, Transformation"
+   "Data Visualization, Dashboards"
+   "Shell Scripting, Git, Regex"
+   "GraphQL, REST / JSON API"
+   "Reactive Web UI"
+   "Graphics, Adobe Creative Suite"])
+
+(def technologies-interested-in
+  {:membrane
+   {:name "Membrane "
+    :url "https://github.com/phronmophobic/membrane"
+    :subtitle "Simple Functional UI Library That Runs Anywhere"}
+   :humbleui
+   {:name "HumbleUI "
+    :url "https://github.com/HumbleUI/HumbleUI"
+    :subtitle "Skia-Based Desktop Functional UI framework"}
+   :hyperfiddle
+   {:name "Hyperfiddle"
+    :url "https://www.hyperfiddle.net/"
+    :subtitle "UI modeling language in a notebook"}
+   :data-rabbit
+   {:name "Data Rabbit"
+    :url "https://www.datarabbit.com/"
+    :subtitle "Flow-based Clojure canvas for data exploration, visualization, & learning. \"A system built for seeing\""}
+   :clerk
+   {:name "Clerk"
+    :url "https://github.com/nextjournal/clerk"
+    :subtitle "Moldable Live Programming for Clojure"}
+   :matrix
+   {:name "Matrix"
+    :url "https://github.com/kennytilton/matrix"
+    :subtitle "Fine-grained, transparent data flow between generative objects."}
+   :flutter/mx
+   {:name "Flutter/MX"
+    :url "https://github.com/kennytilton/flutter-mx"
+    :subtitle "Programming Flutter with ClojureDart and Matrix, a generic, fine-grained, transparent, reactive state manager"}
+   :malli
+   {:name "Malli"
+    :url "https://github.com/metosin/malli"
+    :subtitle "Data-driven Schemas for Clojure/Script"}
+   :babashka
+   {:name "Babashka"
+    :url "https://babashka.org/"
+    :subtitle "Fast native Clojure scripting runtime"}
+   :clojure-dart
+   {:subtitle "Build Dart/Flutter mobile and desktop apps with Clojure"
+    :name "ClojureDart"
+    :url "https://github.com/Tensegritics/ClojureDart"}
+   :meander
+   {:name "Meander"
+    :subtitle "Tools for transparent data transformation"
+    :url "https://github.com/noprompt/meander"}
+   :xtdb
+   {:name "XTDB"
+    :subtitle "Immutable Bitemporal Database for SQL, Datalog & graph queries"
+    :url "https://xtdb.com/"}})
+
+   ;:pathom
+   ;:datalog})
--- src/cv/home.clj
+++ src/cv/home.clj
@@ -3,31 +3,157 @@
     [lambdaisland.hiccup :as hiccup]
     [lambdaisland.ornament :as o :refer [defstyled]]
     [clojure.string :as string]
+    [garden.core :refer [css]]
+    [garden.compiler :as gc :refer [compile-css]]
+    [cv.tw-preflight :refer [tw-preflight-3-css-str]]
     [flatland.useful.seq :refer [partition-between]])
-  (:use [com.rpl.specter]
-        [cv.data]))
+  (:require
+    [cv.data :as data]
+    :reload
+    [garden.selectors :as s])
+  (:use [com.rpl.specter]))
 
 (defstyled title :div
+  :mb-2
+  [:h1 :text-4xl]
   ([]
    [:<>
     [:h1 "Ryan Martin"]
     [:h2 "Full Stack Developer"]]))
 
 (defstyled resume-link :a
+  :block :text-red :text-blue-500 :underline :mb-4
   ([]
    [:<>
     {:href "Resume.pdf"}
-    "Resume (PDF)"]))
+    "PDF Document"]))
 
-(do
-  (def home-hiccup
-    [:html
-     [:head]
-     [:body
-      [title]
-      [resume-link]]])
+(defstyled section-title :h2
+  :text-2xl
+  :font-bold)
 
-  (->> home-hiccup
-    (hiccup/render)
-    (spit "docs/index.html")))
+(defstyled subsection-title :h3
+  :text-lg
+  :font-bold
+  :text-blue-600)
 
+(defstyled sidebar-title subsection-title
+  :mb-2 #_:text-white
+  #_:bg-gray-700)
+
+(defstyled skill-keywords :div
+  :mb-4
+  [:li :mb-6]
+  ([]
+   [:<>
+    [sidebar-title "Additional Skills"]
+    [:ul
+     (for [x data/skill-keywords]
+       [:li x])]]))
+
+
+
+(defstyled experience :div
+  :mb-4
+  [:li :mb-2]
+  [:p :mb-4 :pl-3]
+  ([]
+   [:<>
+    [section-title "Experience"]
+    (for [[t s] data/experience]
+      [:div
+       [subsection-title t]
+       [:p s]])]))
+
+(defstyled education :div
+  :mb-4
+  ([]
+   [:<>
+    [section-title "Education"]
+    [:span data/education]]))
+
+
+(defstyled column :div
+  :h-full :p-4)
+
+(defstyled left-col column
+  :max-w-300px :bg-black :text-white :pl-8)
+
+(defstyled right-col column
+  :bg-white)
+
+(defstyled cols :div
+  :flex :gap-3)
+  ;[(s/> (s/&) (s/div (s/first-child))) :w-33% :bg-gray-400]
+  ;[(s/> (s/&) (s/div (s/last-child)))  :w-77% :bg-red-500]
+  ;([left right]
+  ; [:<>
+  ;  left
+  ;  right]))
+
+(defstyled body :body
+  :p-0 :m-0)
+
+(defstyled cool-technologies :div
+  [:a :font-bold :min-w-24 :underline :text-blue-700]
+  [:.item :flex :gap-8 :mb-2]
+  ([]
+   [:<>
+    [section-title "Technologies I'm interested in"]
+    (for [{lib-name :name :keys [url subtitle]} (vals data/technologies-interested-in)]
+      [:div.item
+       [:a {:href url} lib-name]
+       [:span subtitle]])]))
+
+(defstyled skills-summary :div
+  :mb-8
+  [:.block :mb-4]
+  [:.subtitle :mb-2]
+  [:ul :list-outside]
+  [:li :mb-4]
+  ([]
+   [:<>
+    [section-title "Skills Summary"]
+    (for [[subtitle experience-lines] data/skills-summary]
+      [:div.block
+       [subsection-title {:class "subtitle"} subtitle]
+       [:ul
+        (for [x experience-lines]
+          [:li x])]])]))
+
+;todo: make links live
+(defstyled personal-info :div
+  :mb-8
+  [:.title :font-bold]
+  [:.item :mb-4]
+  ([]
+   [:<>
+    [sidebar-title "Personal Info"]
+    (for [[k v] data/personal-info]
+      [:div.item
+       [:div.title k]
+       [:div v]])]))
+
+(def home-hiccup
+  [:html
+   [:head
+    [:style tw-preflight-3-css-str]
+    [:style (o/defined-styles)]
+    [:script {:src "https://livejs.com/live.js" :type "text/javascript"}]]
+   [body]
+   [cols
+    [left-col
+     [title]
+     [resume-link]
+     [personal-info]
+     [skill-keywords]]
+    [right-col
+     [skills-summary]
+     [experience]
+     [education]
+     [cool-technologies]]]])
+
+;#_
+(->> home-hiccup
+  (hiccup/render)
+  (spit "docs/index.html"))
